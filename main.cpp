ac31b5c6d05cd62ca2837c193c53c47b8c0b61a1
Albert Bertram <bertrama@umich.edu>
2014-10-29 06:26:24 -0400

    Adding options for configuring the Hot Rod client. Adding an option for being notified of errors.

--- main.cpp
+++ main.cpp
@@ -8,18 +8,38 @@
 
 using namespace infinispan::hotrod;
 
-#ifndef DEFAULT_HOTROD_HOST
-#define DEFAULT_HOTROD_HOST "127.0.0.1"
-#endif
+#define HOTROD_DEFAULT_HOST                    "127.0.0.1"
+#define HOTROD_DEFAULT_PORT                    11222
 
-#ifndef DEFAULT_HOTROD_PORT
-#define DEFAULT_HOTROD_PORT 11222
-#endif
+#define HOTROD_VERBOSITY_SILENT                0
+#define HOTROD_VERBOSITY_EXCEPTION             1
+#define HOTROD_VERBOSITY_NOTICE                2
+#define HOTROD_VERBOSITY_WARNING               3
+#define HOTROD_VERBOSITY_ERROR                 4
+
+#define HOTROD_INI_CONNECTION_TIMEOUT          "hotrod.connection_timeout"
+#define HOTROD_INI_FORCE_RETURN_VALUES         "hotrod.force_return_values"
+#define HOTROD_INI_KEY_SIZE_ESTIMATE           "hotrod.key_size_estimate"
+#define HOTROD_INI_PING_ON_STARTUP             "hotrod.ping_on_startup"
+#define HOTROD_INI_PROTOCOL_VERSION            "hotrod.protocol_version"
+#define HOTROD_INI_SOCKET_TIMEOUT              "hotrod.socket_timeout"
+#define HOTROD_INI_TCP_NO_DELAY                "hotrod.tcp_no_delay"
+#define HOTROD_INI_VALUE_SIZE_ESTIMATE         "hotrod.value_size_estimate"
+
+#define HOTROD_INI_DEFAULT_CONNECTION_TIMEOUT  60000
+#define HOTROD_INI_DEFAULT_FORCE_RETURN_VALUES false
+#define HOTROD_INI_DEFAULT_KEY_SIZE_ESTIMATE   64
+#define HOTROD_INI_DEFAULT_PING_ON_STARTUP     true
+#define HOTROD_INI_DEFAULT_PROTOCOL_VERSION    Configuration::PROTOCOL_VERSION_12
+#define HOTROD_INI_DEFAULT_SOCKET_TIMEOUT      60000
+#define HOTROD_INI_DEFAULT_TCP_NO_DELAY        true
+#define HOTROD_INI_DEFAULT_VALUE_SIZE_ESTIMATE 512
 
 class Hotrod : public Php::Base, public Php::ArrayAccess {
 private:
     RemoteCacheManager *_cm = nullptr;
     RemoteCache<std::string, std::string> *_cache = nullptr;
+    int _verbosity = HOTROD_VERBOSITY_SILENT;
 
     Php::Value _get(const Php::Value &key) {
         std::string *pvalue = nullptr;
@@ -28,6 +48,7 @@ private:
                 pvalue = _cache->get(std::string((const char *) key));
             }
             catch (...) {
+                tell("Hotrod::get(): failed");
             }
         }
         if (pvalue == nullptr) {
@@ -46,6 +67,7 @@ private:
                 _cache->put(k, v, (uint64_t) life, (uint64_t) idle);
             }
             catch (...) {
+                tell("Hotrod::put(): failed");
             }
         }
     }
@@ -56,11 +78,54 @@ private:
                 _cache->remove(std::string((const char *) key));
             }
             catch (...) {
+                tell("Hotrod::remove(): failed");
             }
         }
     }
 
+    void tell(std::string message) {
+        switch (_verbosity) {
+        case HOTROD_VERBOSITY_SILENT:
+            break;
+
+        case HOTROD_VERBOSITY_EXCEPTION:
+            throw Exception(message);
+            break;
+
+        case HOTROD_VERBOSITY_NOTICE:
+            Php::notice << message << std::flush;
+            break;
+
+        case HOTROD_VERBOSITY_WARNING:
+            Php::warning << message << std::flush;
+            break;
+
+        case HOTROD_VERBOSITY_ERROR:
+            Php::error << message << std::flush;
+            break;
+        }
+    }
+
+    ConfigurationBuilder initializeBuilder() {
+        ConfigurationBuilder builder;
+        builder.connectionTimeout(Php::ini_get(HOTROD_INI_CONNECTION_TIMEOUT));
+        builder.forceReturnValues(Php::ini_get(HOTROD_INI_FORCE_RETURN_VALUES));
+        builder.keySizeEstimate(Php::ini_get(HOTROD_INI_KEY_SIZE_ESTIMATE));
+        builder.pingOnStartup(Php::ini_get(HOTROD_INI_PING_ON_STARTUP));
+        builder.protocolVersion(Php::ini_get(HOTROD_INI_PROTOCOL_VERSION));
+        builder.socketTimeout(Php::ini_get(HOTROD_INI_SOCKET_TIMEOUT));
+        builder.tcpNoDelay(Php::ini_get(HOTROD_INI_TCP_NO_DELAY));
+        builder.valueSizeEstimate(Php::ini_get(HOTROD_INI_VALUE_SIZE_ESTIMATE));
+        return builder;
+    }
+
 public:
+    static const int SILENT    = HOTROD_VERBOSITY_SILENT;
+    static const int EXCEPTION = HOTROD_VERBOSITY_EXCEPTION;
+    static const int NOTICE    = HOTROD_VERBOSITY_NOTICE;
+    static const int WARNING   = HOTROD_VERBOSITY_WARNING;
+    static const int ERROR     = HOTROD_VERBOSITY_ERROR;
+
     // The constructor doesn't get passed arguments from the new statement in php.
     Hotrod() {}
 
@@ -84,14 +149,17 @@ public:
             hostname = std::string((const char *) params[0]);
         }
         else {
-            hostname = DEFAULT_HOTROD_HOST;
+            hostname = HOTROD_DEFAULT_HOST;
         }
 
         if (params.size() > 1) {
             port = params[1];
         }
         else {
-            port = DEFAULT_HOTROD_PORT;
+            port = HOTROD_DEFAULT_PORT;
+        }
+        if (params.size() > 2) {
+            _verbosity = params[2];
         }
     
         try {
@@ -102,6 +170,7 @@ public:
             _cache = new RemoteCache<std::string, std::string>(_cm->getCache<std::string, std::string>());
         }
         catch (...) {
+            tell("Hotrod::constructor(): failed");
         }
     }
 
@@ -111,6 +180,7 @@ public:
                 return _cache->containsKey(std::string((const char *) key));
             }
             catch (...) {
+                tell("Hotrod::offsetExists(): failed");
             }
         }
         return false;
@@ -145,13 +215,7 @@ public:
     }
 
     void remove(Php::Parameters &params) {
-        if (_cache != nullptr) {
-            try {
-                _cache->remove(params[0]);
-            }
-            catch (...) {
-            }
-        }
+        _remove(params[0]);
     }
 
     void clear() {
@@ -160,6 +224,7 @@ public:
                 _cache->clear();
             }
             catch (...) {
+                tell("Hotrod::clear(): failed");
             }
         }
     }
@@ -170,6 +235,7 @@ public:
                 return Php::Value(_cache->stats());
             }
             catch (...) {
+                tell("Hotrod::stats(): failed");
             }
        }
        return Php::Value(nullptr);
@@ -181,6 +247,7 @@ public:
                 return Php::Value((int64_t) _cache->size());
             }
             catch (...) {
+                tell("Hotrod::size(): failed");
             }
         }
         return Php::Value(nullptr);
@@ -216,7 +283,8 @@ extern "C" {
 
         hotrod.method("__construct", &Hotrod::__construct, {
             Php::ByVal("hostname", Php::Type::String, false),
-            Php::ByVal("port", Php::Type::Numeric, false)
+            Php::ByVal("port", Php::Type::Numeric, false),
+            Php::ByVal("verbosity", Php::Type::Numeric, false)
         });
 
         hotrod.method("put", &Hotrod::put, {
@@ -237,6 +305,15 @@ extern "C" {
         hotrod.method("clear", &Hotrod::clear);
         hotrod.method("stats", &Hotrod::stats);
         hotrod.method("size",  &Hotrod::size);
+
+        extension.add(Php::Ini( HOTROD_INI_CONNECTION_TIMEOUT, HOTROD_INI_DEFAULT_CONNECTION_TIMEOUT ));
+        extension.add(Php::Ini( HOTROD_INI_FORCE_RETURN_VALUES, HOTROD_INI_DEFAULT_FORCE_RETURN_VALUES ));
+        extension.add(Php::Ini( HOTROD_INI_KEY_SIZE_ESTIMATE, HOTROD_INI_DEFAULT_KEY_SIZE_ESTIMATE ));
+        extension.add(Php::Ini( HOTROD_INI_PING_ON_STARTUP, HOTROD_INI_DEFAULT_PING_ON_STARTUP ));
+        extension.add(Php::Ini( HOTROD_INI_PROTOCOL_VERSION, HOTROD_INI_DEFAULT_PROTOCOL_VERSION ));
+        extension.add(Php::Ini( HOTROD_INI_SOCKET_TIMEOUT, HOTROD_INI_DEFAULT_SOCKET_TIMEOUT ));
+        extension.add(Php::Ini( HOTROD_INI_TCP_NO_DELAY, HOTROD_INI_DEFAULT_TCP_NO_DELAY ));
+        extension.add(Php::Ini( HOTROD_INI_VALUE_SIZE_ESTIMATE, HOTROD_INI_DEFAULT_VALUE_SIZE_ESTIMATE ));
 
         extension.add(std::move(hotrod));
         
